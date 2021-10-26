087753ca3c9799d3cdf5f1c5ffc48f6cd7f7ffbc
Aaron Roberts <aaronroberts@Aarons-MacBook-Pro.local>
2021-10-26 09:24:40 -0500

    saving

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+let userInput = prompt("What is your name?")
+if (userInput==="Aaron")prompt("Where will you be going?")
+else if (userInput==="Norway")prompt("What type of food do you prefer?")
+else if (userInput==="Food"||"Lunch"||"food"||"lunch")prompt(
+    "Italian food"||"Spanish Food"||"Russian food")
+
+    
+    
+else if (userInput==="Entertainment")prompt("Great! movie theatre it is!")
+else prompt("Error")
+
+
